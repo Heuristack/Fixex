c8771b925ee677eff7bc27a598cbd5b9d78e729a
Lance LI <heuristack@gmail.com>
2018-08-21 16:42:55 +0800

    Fixex: initialize the project structure

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "quickfix/Utility.h"
+#include "quickfix/Values.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Fixex: a simple exchange based on " << FIX::BeginString_FIX42 << std::endl;
+}
+
